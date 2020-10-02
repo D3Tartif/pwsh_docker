22246ce8a5578416bd4b16eeb5cd0e1a3012f9dd
D3Tartif <flouret.cedric@gmail.com>
2020-10-02 14:04:15 +0200

    header done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pwsh_docker
+
+# 1/ Presentataion
+
+admin menu in powershell to install/remove docker, create/list/remove containers.
+
+
---
+++ main.ps1
@@ -0,0 +1,9 @@
+################################################
+# Docker manage admin console                  #
+# Author: Cedric FLOURET                       #
+# Creation data: 2/1/2020                      #
+# Last change date: 2/1/2020                   #
+# Version: 0.1                                 #
+#                                              #
+#                                              #
+################################################
